7f73038d17841082fe641a612160100e49341c9d
Portmen <piskunov.ilia@mail.ru>
2023-03-10 10:17:57 +0300

    Create button and entry

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 from tkinter import *
+from tkinter import ttk
 from os import *
 
-
+#def create_container()
 
 
 if __name__ == "__main__":
     app = Tk()
     app.title('ContainerAPP')
-    app.geometry("400x400")
-    lable = Label(text='APP')
-    lable.pack()
+    app.geometry("800x800+500+100")
+    icon =  PhotoImage(file="./icon.png")
+
+    app.iconphoto(False, icon)
+    btn = ttk.Button(text="CREATE")
+    btn.pack(anchor="e", side="bottom", ipadx=8, ipady=8, padx=10, pady=10)
+    #btn.bind("<Enter>", create_container)
+
+    entry_arch= ttk.Entry(justify=CENTER)
+    entry_arch.pack(anchor=NE, padx=10, pady= 10)
+
     app.mainloop()
 
